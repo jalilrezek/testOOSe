860e7b3a0beedf7a9127abb5970643dbf85ff78f
jalilrezek <jalilrezek27@gmail.com>
2025-02-02 11:10:10 -0500

    removed the all posts button from header. Adding, deleting, editing posts seems to work. Didn't test comments

--- web/src/components/layout/header.tsx
+++ web/src/components/layout/header.tsx
@@ -36,18 +36,6 @@ const Header = () => {
           onClick={() => setEnableFilter(true)}
         >{`My ${label}`}</Button>
       )}
-      <Button
-        variant={"link"}
-        className={cn({
-          underline: showUserFilter && !enableFilter,
-          "text-primary": !enableFilter,
-          "text-primary/60": enableFilter,
-        })}
-        disabled={!showUserFilter}
-        onClick={() => setEnableFilter(false)}
-      >
-        {`All ${label}`}
-      </Button>
     </div>
   );
 };
